9148f6b26c8c37ed4181c7747b94cb76663f0698
Maria Padalka <ms.padalka@gmail.com>
2024-03-14 20:37:42 +0200

    init project

---
+++ ASPNET-CORE-MVC/Domain/Models/Transaction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Transaction
+    {
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public Guid? FromCategory { get; set; }
+
+        public Guid? ToCategory { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateTime? Date { get; set; }
+
+        public string? Details { get; set; }
+
+        public bool Planned { get; set; }
+
+        public virtual Category? FromCategoryNavigation { get; set; }
+
+        public virtual Category? ToCategoryNavigation { get; set; }
+
+        public virtual User User { get; set; } = null!;
+    }
+}
---
+++ ASPNET-CORE-MVC/Domain/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Domain.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string Email { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+
+        public string Surname { get; set; } = null!;
+
+        public DateTime? BirthDate { get; set; }
+
+        public string Currency { get; set; } = null!;
+
+        public virtual ICollection<Category> Categories { get; set; } = new List<Category>();
+
+        public virtual ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+    }
+}
